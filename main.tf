02bd878fa69efc07fd32c51bc421e1ca4bdce661
Arpita <arpitasingh190597@gmail.com>
2024-05-25 11:56:33 +0530

    one more new rg

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "RG" {
   name     = "hari-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "RG" {
+  name     = "hari-rg1"
+  location = "West Europe"
+}
